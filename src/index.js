37644a275224e04e644813dca6cc61bea8684fff
Kevin Granger <shipowlata@gmail.com>
2021-02-11 17:06:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./style";
+import { Component, render } from "preact";
+import { Result } from "./result";
+
+const SEARCH = "//api.github.com/search/repositories";
+
+export default class App extends Component {
+  componentDidMount() {
+    fetch(`${SEARCH}?q=preact`)
+      .then(r => r.json())
+      .then(json => {
+        this.setState({
+          results: (json && json.items) || []
+        });
+      });
+  }
+
+  render(props, { results = [] }) {
+    return (
+      <div>
+        <h1>Example</h1>
+        <div class="list">
+          {results.map(result => <Result result={result} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+if (typeof window !== "undefined") {
+  render(<App />, document.getElementById("root"));
+}
